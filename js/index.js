86544dfe91a5d1e92b07ed6e38179912a8ad8364
CodingYongjun <108567709+CodingYongjun@users.noreply.github.com>
2023-02-16 10:44:39 +0900

    audio 실행 코드 추가
    게임 스타트를 클릭하면 audio가 실행되는 코드를 추가하였다

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
+
 $(function(){
+    const sound=document.getElementById('intro');
+
     $('#wrapper2').hide();
     $('.start').on('click',function(){
         $('#wrapper').hide();
         $('#wrapper2').show();
+        sound.play();
+        sound.volume=0.4;
     });
-    $(document).on('touchstart',function(){});
     $('.st>a').on('click',function(){
         window.location.href="game.html";
     });
